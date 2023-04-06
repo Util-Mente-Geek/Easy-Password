42d3231e26e004aa52b081d6bce3a1f01b151188
Martin Segoviano <segoviano_martin@hotmail.com>
2023-04-06 15:52:18 -0600

    Configurando contraseña con boton externo

--- Cambiando_contrase_a_con_PushButton_en_Arduino/Cambiando_contrase_a_con_PushButton_en_Arduino.ino
+++ Cambiando_contrase_a_con_PushButton_en_Arduino/Cambiando_contrase_a_con_PushButton_en_Arduino.ino
@@ -1,6 +1,6 @@
 /** AxeDoor
- * Codigo para teclear una contraseña predefinida en programa
- * Microcontrolador: Arduino uno
+ * Codigo para configurar contraseña con pushbutton y teclado matricial 2x2
+ * Microcontrolador: Arduino
  * Escrito por: Útil Mente Geek
  * PaginaWeb: www.utilmentegeek.com
  * YouTube: https://www.youtube.com/channel/UCBqfcl_tue6Zsk4Hc3BErWg
@@ -11,6 +11,7 @@
  * en nuestras redes socilaes @Util Mente Geek.
  */
   #include <Keypad.h>        
+       
 
   const byte FILAS = 2;      
   const byte COLUMNAS = 2;   
@@ -21,25 +22,32 @@
   
   byte pinesFilas[FILAS] = {13,12}; 
   byte pinesColumnas[COLUMNAS] = {11,10};
-  
   Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS); 
   
   char TECLA; 
   char CLAVE[5]; 
-  char CLAVE_MAESTRA[5] = "4312"; 
+  char CLAVE_MAESTRA[5]; 
   byte INDICE = 0; 
+  byte INDICE2 = 0;
   int ledC = 9;    
   int ledI = 8;   
-  
+  int btnConf = 7; 
+  int ledN = 6;   
   
   void setup() {
     Serial.begin(9600); 
     pinMode(ledC, OUTPUT); 
     pinMode(ledI, OUTPUT);  
+    pinMode(ledN, OUTPUT);  
+    pinMode(btnConf, INPUT); 
+  
                 }
   
   void loop() {
-                     
+    if (digitalRead(btnConf)){ 
+      configuracion();        
+         }
+      else {            
           TECLA = teclado.getKey(); 
       if (TECLA) {    
         CLAVE[INDICE] = TECLA; 
@@ -62,4 +70,30 @@
        INDICE = 0;  
            }
          }
-     
+      
+  }
+  void configuracion() { 
+    
+    memset(CLAVE_MAESTRA, 0, 5); 
+    memset(CLAVE, 0, 5);    
+    INDICE = 0;  
+    INDICE2 = 0;    
+    while (digitalRead(btnConf)) {  
+          TECLA = teclado.getKey(); 
+      if (TECLA){  
+        CLAVE_MAESTRA[INDICE2] = TECLA; 
+        INDICE2++; 
+        Serial.print(TECLA); 
+                }
+                
+      if (INDICE2 == 4) {  
+         INDICE2 = 0;  
+         Serial.println(" CAMBIADA"); 
+                           }
+          digitalWrite(ledN, HIGH);                  
+                      }
+                      
+      digitalWrite(ledN, LOW);
+                  }
+      
+              
